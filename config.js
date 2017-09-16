5e2d6fa02bbb9999a9042905a973d9f8b854a2cf
synergico <tri.nguyen@synergi.co>
2017-09-16 17:04:44 +0100

    a

--- config.js
+++ config.js
@@ -11,7 +11,7 @@ var doc = docute.init({
     ],
     nav: [
         // homepage
-        {title: 'Home', path: '/README'},
+        {title: 'Home', path: '/'},
         {title: 'Setting', path: '/settings/settings'},
         {title: 'Work Station', path: '/docs/list'}
     ],
